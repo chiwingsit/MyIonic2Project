701e487dc85ef83392fc46cc355ec442ad75b43f
chiwingsit <chi-wing.sit@mail.mcgill.ca>
2017-01-29 21:50:02 -0500

    Add camera plugin

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { ModalController } from 'ionic-angular';
 
+import { Camera } from 'ionic-native';
+
 import { ModalContentPage } from '../log-entry/log-entry';
 
 @Component({
@@ -9,6 +11,8 @@ import { ModalContentPage } from '../log-entry/log-entry';
   templateUrl: 'hello-ionic.html'
 })
 export class HelloIonicPage {
+  public base64Image: string;
+
   constructor(public modalCtrl: ModalController) {
 
   }
@@ -17,4 +21,16 @@ export class HelloIonicPage {
     let modal = this.modalCtrl.create(ModalContentPage);
     modal.present();
   }
+
+  takePicture(){
+    Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
+  }
+
 }
